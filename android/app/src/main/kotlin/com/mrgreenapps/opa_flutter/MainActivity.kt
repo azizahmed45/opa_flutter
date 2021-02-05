215e3145ce16c62bc3e5788d454d61fa2609571c
Aziz Ahmed <azizahmedsabuj@gmail.com>
2021-02-06 01:55:07 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/mrgreenapps/opa_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrgreenapps.opa_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          LogoChar(
+            char: "O",
+            color: Colors.teal,
+          ),
+          LogoChar(
+            char: "P",
+            color: Colors.orangeAccent,
+          ),
+          LogoChar(
+            char: "A",
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LogoChar extends StatelessWidget {
+  const LogoChar({this.char, this.color});
+
+  final String char;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      char,
+      style: TextStyle(
+          color: color,
+          fontSize: MediaQuery.of(context).size.width / 3,
+          fontWeight: FontWeight.bold),
+    );
+  }
+}
